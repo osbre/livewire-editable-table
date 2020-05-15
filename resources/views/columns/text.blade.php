aee1505eebe635515cfa5959e4844db7e3795711
ostap <osbre@protonmail.com>
2020-05-16 01:56:28 +0300

    add method "render"

--- resources/views/columns/text.blade.php
+++ resources/views/columns/text.blade.php
@@ -1,7 +1,11 @@
 <td class="p-0">
-    <input type="text"
-           x-model="{{ $key }}"
-           x-on:change="update({{ $eloquentKey }}, '{{ $column }}')"
-           value="{{ $value }}"
-           class="form-control mw-100 border-0 no-resize">
+    @isset($render)
+        {!! $render !!}
+    @else
+        <input type="text"
+               x-model="{{ $key }}"
+               x-on:change="update({{ $eloquentKey }}, '{{ $column }}')"
+               value="{{ $value }}"
+               class="form-control mw-100 border-0 no-resize">
+    @endisset
 </td>
--- src/Columns/Text.php
+++ src/Columns/Text.php
@@ -2,7 +2,26 @@
 
 namespace Ostap\EditableTable\Columns;
 
+use Closure;
+
 class Text extends Column
 {
     public static string $view = 'editable-table::columns.text';
+
+    protected ?Closure $render = null;
+
+    public function render(Closure $render)
+    {
+        $this->render = $render;
+        return $this;
+    }
+
+    public function tdData(array $model): array
+    {
+        return array_merge(parent::tdData($model), [
+            'render' => $this->render
+                ? with(parent::tdData($model), $this->render)
+                : null,
+        ]);
+    }
 }
